7bb690f75459d7c1f351683c92990b9ec290807d
castellErika <castell.erika@gmail.com>
2018-11-12 20:14:44 -0500

    Add files via upload

--- js/detalleProducto.js
+++ js/detalleProducto.js
@@ -1,7 +1,9 @@
     var section = document.querySelector('.product-details');
     var pathname = window.location.hash;
     var urlDetalle = pathname.split("#");
-    console.log(urlDetalle);
+    var captureDetalle = urlDetalle[1];
+
+    console.log(captureDetalle);
 
    function loadJSON(callback) {   
 
@@ -38,65 +40,61 @@
 
             var iDiv = document.createElement('div');
             iDiv.id = 'detail';
-            if(urlDetalle == _id){
-            iDiv.className = 'carousel-item col-md-4 active'; 
+            if(captureDetalle == _id){
+	            iDiv.className = 'row'; 
+	            section.appendChild(iDiv);
+
+	            var figure = document.createElement('div');
+	            iDiv.id = 'image';
+	            figure.className = 'col-lg-6 py-3 order-2 order-lg-1';
+	            iDiv.appendChild(figure);
+
+	            var imagen = document.createElement('img');
+	            imagen.src = picture;
+	            figure.appendChild(imagen);
+
+	            var txtProd = document.createElement('div');
+	            txtProd.className = 'd-flex align-items-center col-lg-6 col-xl-5 pl-lg-5 mb-5 order-1 order-lg-2';
+	            iDiv.appendChild(txtProd);
+
+	            var contenedor = document.createElement('div');
+	            txtProd.appendChild(contenedor);
+
+	            var title_prod = document.createElement('h1');
+	            title_prod.className = 'mb-4';
+	            var node = document.createTextNode(title);
+	            title_prod.appendChild(node);
+	            contenedor.appendChild(title_prod);
+
+	            var indProd = document.createElement('div');
+	            indProd.className = 'd-flex flex-column flex-sm-row align-items-sm-center justify-content-sm-between mb-4';
+	            contenedor.appendChild(indProd);
+
+	            var cate_prod = document.createElement('span');
+	            cate_prod.className = 'badge badge-primary';
+	            var node = document.createTextNode(category);
+	            cate_prod.appendChild(node);
+	            indProd.appendChild(cate_prod);
+
+	            var contReg = document.createElement('div');
+	            contReg.className = 'd-flex align-items-center';
+	            indProd.appendChild(contReg);
+
+	            var textRegis = document.createElement('span');
+	            textRegis.className = 'badge badge-light';
+	            var node = document.createTextNode(registered);
+	            textRegis.appendChild(node);
+	            contReg.appendChild(textRegis);
+
+	            var descripcion_prod = document.createElement('p');
+	            descripcion_prod.className = 'mb-4 text-muted';
+	            var node = document.createTextNode(about);
+	            descripcion_prod.appendChild(node);
+	            contenedor.appendChild(descripcion_prod);
+	            break;
             }else{
-            iDiv.className = 'row '; 
+            iDiv.className = 'row'; 
             }
-            section.appendChild(iDiv);
-
-            var figure = document.createElement('div');
-            iDiv.id = 'image';
-            figure.className = 'col-lg-6 py-3 order-2 order-lg-1';
-            iDiv.appendChild(figure);
-
-            var imagen = document.createElement('img');
-            imagen.src = picture;
-            figure.appendChild(imagen);
-
-            var txtProd = document.createElement('div');
-            txtProd.className = 'd-flex align-items-center col-lg-6 col-xl-5 pl-lg-5 mb-5 order-1 order-lg-2';
-            iDiv.appendChild(txtProd);
-
-            var contenedor = document.createElement('div');
-            txtProd.appendChild(contenedor);
-
-            var title_prod = document.createElement('h1');
-            title_prod.className = 'mb-4';
-            var node = document.createTextNode(title);
-            title_prod.appendChild(node);
-            contenedor.appendChild(title_prod);
-
-            var indProd = document.createElement('div');
-            indProd.className = 'd-flex flex-column flex-sm-row align-items-sm-center justify-content-sm-between mb-4';
-            contenedor.appendChild(indProd);
-
-            var cate_prod = document.createElement('span');
-            cate_prod.className = 'badge badge-primary';
-            var node = document.createTextNode(category);
-            cate_prod.appendChild(node);
-            indProd.appendChild(cate_prod);
-
-            var contReg = document.createElement('div');
-            contReg.className = 'd-flex align-items-center';
-            indProd.appendChild(contReg);
-
-            var textRegis = document.createElement('span');
-            textRegis.className = 'badge badge-light';
-            var node = document.createTextNode(registered);
-            textRegis.appendChild(node);
-            contReg.appendChild(textRegis);
-
-            var descripcion_prod = document.createElement('p');
-            descripcion_prod.className = 'mb-4 text-muted';
-            var node = document.createTextNode(about);
-            descripcion_prod.appendChild(node);
-            contenedor.appendChild(descripcion_prod);
-
-            var vinculo = document.createElement('a');
-            vinculo.href = 'detalle.html#'+_id;
-            vinculo.className = 'card-vinculo';
-            card.appendChild(vinculo);
 
         }
     }
